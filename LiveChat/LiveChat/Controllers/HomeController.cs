1ade962ffe87633839c4f4401376be5446bffed9
Klind Beqiri <24981411+Klind23@users.noreply.github.com>
2019-09-02 18:00:05 +0200

    Improved stuff

--- LiveChat/LiveChat/Controllers/HomeController.cs
+++ LiveChat/LiveChat/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using LiveChat.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace LiveChat.Controllers
 {
@@ -7,6 +9,35 @@ namespace LiveChat.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(PerdoruesModel perdorues)
+        {
+            using (var context = new LiveChatContext())
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View("Login", perdorues);
+                }
+
+                var userDetails = context.Perdorues.Where(x => x.Username == perdorues.Username && x.Password == perdorues.Password).FirstOrDefault();
+
+                if (userDetails == null)
+                {
+                    return View("Login", perdorues);
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserId;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
         }
     }
 }
