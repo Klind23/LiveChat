129080c9a75784e5d4badb72bd08f71a5bb6742c
Klind Beqiri <24981411+Klind23@users.noreply.github.com>
2019-09-02 11:53:08 +0200

    added models and controller

---
+++ LiveChat/LiveChat/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using LiveChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LiveChat.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(PerdoruesModel perdorues)
+        {
+            using (var context = new LiveChatContext())
+            {
+                var userDetails = context.Perdorues.Where(x => x.Username == perdorues.Username && x.Password == perdorues.Password).First();
+                if (userDetails == null)
+                {
+                    return View("Index", perdorues);
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserId;
+                    return RedirectToAction("Index", "Chat");
+                }
+            }
+        }
+    }
+}
---
+++ LiveChat/LiveChat/Models/LiveChatContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LiveChat.Models
+{
+    public class LiveChatContext : IdentityDbContext
+    {
+        public LiveChatContext() : base("LiveChatContext")
+        {
+
+        }
+
+        public static LiveChatContext Create()
+        {
+            return new LiveChatContext();
+        }
+
+        public DbSet <PerdoruesModel> Perdorues { get; set; }
+    }
+}
