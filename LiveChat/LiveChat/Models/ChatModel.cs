f3e4755487e36070d10a9c6f8cd5e8d3edec9792
Klind Beqiri <24981411+Klind23@users.noreply.github.com>
2019-09-09 17:30:12 +0200

    WIP

---
+++ LiveChat/LiveChat/Models/ChatModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LiveChat.Models
+{
+    public class ChatModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public PerdoruesModel Perdorues { get; set; }
+        public int PerdoruesId { get; set; }
+        public DateTime KohaMesazhit { get; set; }
+        public string Mesazhi { get; set; }
+    }
+}
